d498dc11e9b4de1025a98f8f10aeb2fb893f446d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-07 20:39:11 +0200

    Hide password and timestamps in '/api/users' GET route response

--- routes.js
+++ routes.js
@@ -20,7 +20,10 @@ function asyncHandler(cb){
 router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
     const user = req.currentUser;
     res.json({
-        user
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        emailAddress: user.emailAddress
     });
 }));
 
