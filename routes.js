31396705c926a7834de2dce6c12b0e4112dd43af
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-07 19:58:22 +0200

    Add '/api/users' POST route

--- routes.js
+++ routes.js
@@ -1,5 +1,9 @@
+'use strict';
+
 const express = require('express');
+const { User } = require('./models');
 const router = express.Router();
+
 
 function asyncHandler(cb){
     return async (req,res, next) => {
@@ -10,5 +14,21 @@ function asyncHandler(cb){
         }
     }
 }
+
+// Route that creates a new user.
+router.post('/users', asyncHandler(async (req, res) => {
+    console.log(req.body)
+    try {
+        await User.create(req.body);
+        res.status(201).location('/').json({ "message": "Account successfully created!" }).end();
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        }
+    }
+}));
 
 module.exports = router;
