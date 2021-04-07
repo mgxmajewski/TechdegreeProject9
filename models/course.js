a55aaefd36c8e121d836ae8e93477041401614c4
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-07 17:45:59 +0200

    Add Course model

---
+++ models/course.js
@@ -0,0 +1,55 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class Course extends Model {}
+    Course.init({
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    // custom error message
+                    msg: 'Please provide a value for "title"',
+                }
+            },
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    // custom error message
+                    msg: 'Please provide a value for "description"',
+                }
+            },
+        },
+        estimatedTime: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    // custom error message
+                    msg: 'Please provide a value for "estimatedTime"',
+                }
+            },
+        },
+        materialsNeeded: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    // custom error message
+                    msg: 'Please provide a value for "materialsNeeded"',
+                }
+            },
+        }
+    }, {
+        sequelize,
+        modelName: 'Course',
+    });
+
+    return Course;
+}
